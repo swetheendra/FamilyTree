01349c7448aa88f0a1f07f6cd5d341e90a9c2913
unknown <swetheendra.t@gmail.com>
2023-05-15 23:55:24 -0600

    change css for mobile

--- src/util/config.js
+++ src/util/config.js
@@ -1,7 +1,7 @@
 const config = {}
 
 config.endpoint = 'https://nosql-db-tre.documents.azure.com:443/'
-config.key = ''
+config.key = 'RbENL368pHMwtRmrMu0btLk8fFPtfrvu2kx9SYrzuIi5MlLEt82dekaRzn87Lbaa5izIMkTGsFXnACDbQMlySA=='
 
 config.database = {
   id: 'FamilyTree'
