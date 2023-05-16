0c4c5c0ae8a0bbc6de34294e1e52024961bbae7c
unknown <swetheendra.t@gmail.com>
2023-05-15 23:42:06 -0600

    read from db

--- src/util/config.js
+++ src/util/config.js
@@ -1,7 +1,7 @@
 const config = {}
 
 config.endpoint = 'https://nosql-db-tre.documents.azure.com:443/'
-config.key = 'RbENL368pHMwtRmrMu0btLk8fFPtfrvu2kx9SYrzuIi5MlLEt82dekaRzn87Lbaa5izIMkTGsFXnACDbQMlySA=='
+config.key = ''
 
 config.database = {
   id: 'FamilyTree'
